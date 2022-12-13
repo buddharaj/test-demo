75b6c87a4ff0474054b83acd470263602380dc6f
buddharaj ambhore <buddharaj.ambhore@accenture.com>
2022-12-13 11:29:26 +0000

    test chnages

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,10 @@
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 import { Construct } from 'constructs';
+
 export declare class BatchesConsumerFunction extends NodejsFunction {
     constructor(scope: Construct);
+    const x = 10
 }
+
+
+
